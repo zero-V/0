7577118755b504956cf612853ea5b615290cffa3
liuyang <2233035381@qq.com>
2019-11-19 20:46:47 +0800

    2019.11.19

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,24 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-
+import login from "../login/login.vue"
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
+    redirect: "/login"
+  },
+  {
+    path: "/",
+    name: "login",
+    component: login
+  },
+  {
+    path: "/home",
     name: "home",
     component: Home
   },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
 ];
 
 const router = new VueRouter({
@@ -26,5 +26,28 @@ const router = new VueRouter({
   base: process.env.BASE_URL,
   routes
 });
-
+router.beforeEach((to, from, next) => {
+    if (to.path === '/login') {
+      next();
+    } else {
+      let token = localStorage.getItem('Authorization');
+   
+      if (token === 'null' || token === '') {
+        next('/login');
+      } else {
+        next();
+      }
+    }
+  });
+  axios.interceptors.request.use(
+      config => {
+        if (localStorage.getItem('Authorization')) {
+          config.headers.Authorization = localStorage.getItem('Authorization');
+        }
+     
+        return config;
+      },
+      error => {
+        return Promise.reject(error);
+      });
 export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,23 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
+  state: {
+    Authorization: localStorage.getItem('Authorization') ? localStorage.getItem('Authorization') : ''
+  },
+  getters: {
+    // getSortage:function (state) {
+    //   if(!state.token){
+    //     state.token =JSON.parse(localStorage.getItem(key))
+    //   }
+    //   return state.token
+    // }
+  },
+  mutations: {
+    changeLogin (state, user) {
+            state.Authorization = user.Authorization;
+            localStorage.setItem('Authorization', user.Authorization);
+          }
+  },
   actions: {},
   modules: {}
 });
